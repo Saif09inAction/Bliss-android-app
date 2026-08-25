b6c224ad1508493419d5b83419fcb5c432bfcd79
SAIF SALMANI <saif@Saifs-MacBook-Air.local>
2026-08-25 22:47:42 +0530

    fix: bump Room DB version to fix startup crash after adding salaryRemaining to EmployeeEntity

--- app/src/main/java/com/laiza/worker/core/local/LaizaDatabase.kt
+++ app/src/main/java/com/laiza/worker/core/local/LaizaDatabase.kt
@@ -19,7 +19,7 @@ import com.laiza.worker.core.local.entity.*
         NotificationEntity::class,
         SaleRecordEntity::class
     ],
-    version = 11,
+    version = 12,
     exportSchema = false
 )
 abstract class LaizaDatabase : RoomDatabase() {
